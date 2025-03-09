7142b6c0f272ac6e61f26cae441abd1047f43c47
smacpherson64 <smacpherson64@gmail.com>
2025-03-09 18:12:41 -0500

    adds example

--- ssg/utils/remark-plugins/github-directives-to-html.tsx
+++ ssg/utils/remark-plugins/github-directives-to-html.tsx
@@ -6,12 +6,23 @@ export function githubDirectivesToHtml() {
     file: import("npm:vfile").VFile
   ): undefined => {
     visit(tree, function (node) {
+      console.log({ node });
       if (node.type === "containerDirective") {
         const data = node.data || (node.data = {});
         data.hName = "blockquote";
         data.hProperties = {
           class: [node.name].filter(Boolean).join(" ").toLocaleLowerCase(),
         };
+        node.children.unshift({
+          type: "paragraph",
+          data: { hProperties: { class: "header" } },
+          children: [
+            {
+              type: "text",
+              value: node.name,
+            },
+          ],
+        });
       }
     });
   };
